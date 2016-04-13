606fe568ba407c00b06f73fd7860ef9f722092b9
Templeton Tsai <qpzmy701117@gmail.com>
2016-04-13 22:53:16 +1000

    Adding exmaple playbook for installing package

--- main.sh
+++ main.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+ssh_public_key='/home/templeton/Cluster_Cloud_Computing/assignmnet2'
 #Creating Instances, attaching volumes
-echo "Calling boto script"
-python boto_main.py
-echo "Init done!"
+#echo "Calling boto script"
+#python boto_main.py
+#echo "Init done!"
 
 #Installing, configuring packages
+#Checking if ansible exists or not
+command -v ansible-playbook >/dev/null && continue || { echo "ansible-playbook command not found."; exit 1;}
+
+ansible-playbook *.yml
+
 
 
